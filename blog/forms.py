1b9a7e6af60b1fb477db878bcd42dbc5cfc243c5
Mel-Rocha <rochamel73@gmail.com>
2024-04-07 09:35:52 -0300

    comments and posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,24 @@
 from django import forms
 
+from blog.models import Comment
+
 
 class EmailPostForm(forms.Form):
+    """
+    Form for forms that are independent of models and
+    generally represent relationship attributes
+    """
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    """
+    ModelForm for forms associated with models
+    """
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
-from blog.models import Post
-from blog.forms import EmailPostForm
+from blog.models import Post, Comment
+from blog.forms import EmailPostForm, CommentForm
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
 
 
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, \
-                                   status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED)
     sent = False
 
     if request.method == 'POST':
@@ -51,7 +67,10 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
