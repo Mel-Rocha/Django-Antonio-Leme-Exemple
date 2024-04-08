4a9bfb5c2955685ad017b7039f262f8b047ee82c
Mel-Rocha <rochamel73@gmail.com>
2024-04-08 10:56:22 -0300

    search version initial

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,8 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, SearchRank
 from taggit.models import Tag
 
 from blog.models import Post, Comment
@@ -125,9 +126,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
